af03fa01a4e88585e3b73bd8330bacfbf09a7ddd
Felipe Antonio <felipe.antonio.online@proton.me>
2022-06-09 12:54:08 -0300

    Altera descrições e variáveis para maior claridade

--- 01/crescimento.py
+++ 01/crescimento.py
@@ -5,7 +5,7 @@
 def pop_final(pop_i, taxa_c, tempo):
     """Fórmula para calcular crescimento populacional.
     pop_i = população inicial | taxa_c = taxa de crescimento em anos
-    | tempo = anos decorridos | pop_f = população final"""
+    | tempo = anos decorridos"""
 
     pop_f = pop_i * ((1 + taxa_c) ** tempo)
     return pop_f
@@ -14,12 +14,14 @@ def pop_final(pop_i, taxa_c, tempo):
 def pop_iguala(pop_1, taxa_c_1, pop_2, taxa_c_2):
     """Calcula o tempo em anos em que as duas populações se igualam ou uma
     ultrapassa o valor da outra.
-    pop_1 = população menor | pop_2 = população maior"""
+    pop_1 = população menor | pop_2 = população maior
+    Retorna: pop_1_f, pop_2_f e tempo | população final para as duas cidades e
+    o tempo em anos para atingir esse estado."""
 
     tempo = 1
     while pop_1 < pop_2:
-        pop_1 = pop_final(pop_1, taxa_c_1, tempo)
-        pop_2 = pop_final(pop_2, taxa_c_2, tempo)
+        pop_1_f = pop_final(pop_1, taxa_c_1, tempo)
+        pop_2_f = pop_final(pop_2, taxa_c_2, tempo)
         tempo = +1
     tempo = -1
-    return pop_1, pop_2, tempo
+    return pop_1_f, pop_2_f, tempo
