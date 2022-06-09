a8c355e95222048fde6b5a6fba079236994f82ea
Felipe Antonio <felipe.antonio.online@proton.me>
2022-06-09 17:22:49 -0300

    projeto 2 resolvido

--- 02/main.py
+++ 02/main.py
@@ -1,1 +1,9 @@
 """Programa pede cinco números e retorna o maior deles."""
+
+print("Descubra Qual É O Maior Número Dentre Cinco\n")
+num1, num2, num3, num4, num5 = input(
+    "Insira cinco números separados por espaço: "
+).split()
+num_list = [num1, num2, num3, num4, num5]
+num_list = map(int, num_list)
+print(f"O maior número é: {max(num_list)}")
