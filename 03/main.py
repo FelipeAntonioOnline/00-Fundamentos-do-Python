359ff36e58d50a72edd29617ced9f65846434188
Felipe Antonio <felipe.antonio.online@proton.me>
2022-06-13 10:03:50 -0300

    projeto concluído

--- 03/main.py
+++ 03/main.py
@@ -1,1 +1,17 @@
 """Programa pede números e realiza a média aritimética entre eles."""
+
+escolha = "s"
+soma = 0
+partes = 0
+
+print("Vamos Calcular A Média Aritimética\n")
+
+while escolha == "s":
+    numero = int(input("Acrescente um número: "))
+    soma += numero
+    partes += 1
+    escolha = input("Deseja acrescentar outro número? (s/n)")
+    escolha.lower()
+
+media_ari = soma / partes
+print(f"A média é: {media_ari}")
